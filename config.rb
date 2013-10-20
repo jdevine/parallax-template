6773c77e697f630fb91a42cd4fb28b0ab5487726
Takashi Hasuo <t.hasuo@everyleaf.com>
2013-10-20 11:16:52 +0900

    Change exported css's style.

--- config.rb
+++ config.rb
@@ -3,9 +3,10 @@
 ###
 
 # Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
+compass_config do |config|
+  config.output_style = :expanded
+  config.line_comments = false
+end
 
 ###
 # Page options, layouts, aliases and proxies
