101d8fddb04c87a0f75fce7d876558739e7870b8
Takashi Hasuo <t.hasuo@everyleaf.com>
2013-10-27 17:14:33 +0900

    Site updated to 7769d44

--- javascripts/all.js
+++ javascripts/all.js
@@ -12006,6 +12006,23 @@ var ABalytics = {
 
   _gaq.push(["_trackPageview"]);
 
+  (function(i, s, o, g, r, a, m) {
+    i["GoogleAnalyticsObject"] = r;
+    i[r] = i[r] || function() {
+      return (i[r].q = i[r].q || []).push(arguments_);
+    };
+    i[r].l = 1 * new Date();
+    a = s.createElement(o);
+    m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    return m.parentNode.insertBefore(a, m);
+  })(window, document, "script", "//www.google-analytics.com/analytics.js", "ga");
+
+  ga("create", "UA-31136981-3", "hassaku.github.io");
+
+  ga("send", "pageview");
+
 }).call(this);
 (function() {
   window.onload = function() {
