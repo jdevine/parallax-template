172065d197a65b2634ab8ab2dbd3d705be1bde65
Takashi Hasuo <t.hasuo@everyleaf.com>
2013-10-27 19:16:12 +0900

    Site updated to 199c270

--- javascripts/ga.js
+++ javascripts/ga.js
@@ -19,21 +19,14 @@
 
   _gaq.push(["_trackPageview"]);
 
-  (function(i, s, o, g, r, a, m) {
-    i["GoogleAnalyticsObject"] = r;
-    i[r] = i[r] || function() {
-      return (i[r].q = i[r].q || []).push(arguments);
-    };
-    i[r].l = 1 * new Date();
-    a = s.createElement(o);
-    m = s.getElementsByTagName(o)[0];
-    a.async = 1;
-    a.src = g;
-    return m.parentNode.insertBefore(a, m);
-  })(window, document, "script", "//www.google-analytics.com/analytics.js", "ga");
-
-  ga("create", "UA-31136981-3", "hassaku.github.io");
-
-  ga("send", "pageview");
+  (function() {
+    var ga, s;
+    ga = document.createElement("script");
+    ga.type = "text/javascript";
+    ga.async = true;
+    ga.src = ("https:" === document.location.protocol ? "https://ssl" : "http://www") + ".google-analytics.com/ga.js";
+    s = document.getElementsByTagName("script")[0];
+    return s.parentNode.insertBefore(ga, s);
+  })();
 
 }).call(this);
